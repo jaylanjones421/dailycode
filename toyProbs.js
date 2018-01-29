fe1801fc4de9dc9714485aa138e76600aec21c24
Jaylan Jones <jaylanjones421@gmail.com>
2018-01-28 21:28:32 -0600

    new tp

--- toyProbs.js
+++ toyProbs.js
@@ -101,4 +101,11 @@ var descending = x =>{
 //
 //6
 //
-//
+//Write a function that accepts a string, and returns true if it is in the form of a phone number. 
+// Assume that any integer from 0-9 in any of the spots will produce a valid phone number.
+
+// Only worry about the following format:
+// (123) 456-7890 (don't forget the space after the close parentheses) 
+
+var validPhoneNumber=(n)=> (n[0]==='('&& n[4]===')'&& n[9]==='-' && n.length===14)
+
