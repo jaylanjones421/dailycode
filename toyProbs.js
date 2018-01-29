4c3b596324851bd1583259550574953f9f65fa07
Jaylan Jones <jaylanjones421@gmail.com>
2018-01-29 12:17:43 -0600

    new tp

--- toyProbs.js
+++ toyProbs.js
@@ -125,3 +125,31 @@ function oddOrEven(array) {
    return array.reduce((x,i)=>x+i)%2===0 ? 'even':'odd';
   }
   }
+//
+//
+//
+//8
+//
+//
+// Trolls are attacking your comment section!
+
+// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+
+var trollFilter=(str)=>{
+  var vowels = ['a','e','i','o','u','A','E','I','O','U'];
+  var arr = str.split('');
+  arr.map((x,i)=>{
+    if(vowels.includes(x)){
+      arr[i]='';
+    }
+  });
+
+ return arr.join('');
+};
+//
+//
